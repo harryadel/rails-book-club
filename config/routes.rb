3a7936de14316906d6a2a6a7232164d07f66e3ba
Harry Adel <harryadelb@gmail.com>
2020-07-21 19:33:10 +0200

    Change root_path and layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  root 'home#index'
+  root 'posts#index'
 
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
 
   resources :sessions, only: %i[new create destroy]
-  resources :users, only: %i[index new create destroy]
+  resources :users, only: %i[new create destroy]
   resources :posts do
     resources :comments
   end
